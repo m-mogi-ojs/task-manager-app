05adabbcf73929af3423709ae74ccdc311bd4e3a
m-mogi-ojs <m.mogi.ojs@gmail.com>
2019-05-27 00:14:43 +0900

    add login

--- web/app/task-manager-app/app/models/user.rb
+++ web/app/task-manager-app/app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   has_many :kanbans
+  before_save :downcase_email
+  validates :email, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
+  
   has_secure_password
 
   def User.digest(string)
@@ -7,4 +11,25 @@ class User < ApplicationRecord
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
+
+  def forget
+   update_attribute(:remember_digest, nil)
+  end
+
+  private 
+  
+    def downcase_email
+      self.email.downcase!
+    end
 end
