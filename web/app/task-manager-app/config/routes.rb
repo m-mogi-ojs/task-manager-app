04c3cd18f89219883c559adf52c33cfadb65f378
m-mogi-ojs <m.mogi.ojs@gmail.com>
2019-05-26 22:36:21 +0900

    add signup

--- web/app/task-manager-app/config/routes.rb
+++ web/app/task-manager-app/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
   root 'top#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
