752c4a4fd614fef4c78dad5012365a840fc0ccc5
m-mogi-ojs <m.mogi.ojs@gmail.com>
2019-06-24 00:36:07 +0900

    add comment

--- web/app/task-manager-app/app/assets/javascripts/kanbans.js
+++ web/app/task-manager-app/app/assets/javascripts/kanbans.js
@@ -1,16 +1,14 @@
 $(function(){
-  console.log("Hello JQuery");
-
+  // タスク名クリック時にテキストボックスを表示
   $(".task").click(function() {
-    console.log($(this).text());
     var $task = $(this);
     $task.hide();
     $task.parent().find('.task-edit').val($task.text()).show().focus();
   });
-
+  // タスク名テキストボックスからフォーカスを外した際にタスク名に反映
   $(".task-edit").blur(function() {
-    console.log($(this).val());
     var $taskEdit = $(this);
+    // ajaxでデータ更新
     $taskEdit.hide()
     $taskEdit.parent().find('.task').text($taskEdit.val()).show();
   });
